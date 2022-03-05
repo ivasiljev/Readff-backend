a7ad78e771318df87d621c3272d576d81fb5865c
Ivan Vasiljev <ivasiljev@internet.ru>
2022-03-05 14:03:04 +0300

    Init

---
+++ src/main/java/com/readff/readff_backend/ReadffBackendApplication.java
@@ -0,0 +1,13 @@
+package com.readff.readff_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadffBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadffBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/readff/readff_backend/ReadffBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readff.readff_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadffBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
